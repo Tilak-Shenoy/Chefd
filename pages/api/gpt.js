445091764170decd7d4c809eca896554ea03445b
Tilak N Shenoy <tilaknshenoy@gmail.com>
2023-05-15 19:46:59 -0500

    Update gpt.js

--- pages/api/gpt.js
+++ pages/api/gpt.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.NEXT_OPENAI_API_KEY,
+  apiKey: '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'
 });
 
 const openai = new OpenAIApi(configuration);
