761e8f7b2c8ca0384ac9bde0f20da7fcdbfecf23
Tilak Shenoy <tilakshenoy@Tilaks-MacBook-Air.local>
2023-07-13 23:33:07 -0500

    fixed middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 export default function middleware(req) {
    if(req.nextUrl.pathname =="/api/gpt"){
-      if(req.nethod != 'POST'){
+      if(req.method != 'POST'){
        return new NextResponse("Cannot access this endpoint with " + req.method, { status: 400})
       }
    return NextResponse.next();
